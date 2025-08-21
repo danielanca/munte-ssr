605f8a56637cd903a0a3d9fd91c80076454b9594
Daniel Anca <daniel.anca@eggs.ro>
2025-08-21 21:51:01 +0300

    Updates for the project.

--- src/client/declara.d.ts
+++ src/client/declara.d.ts
@@ -1,1 +1,12 @@
 declare module "shards-react";
+
+declare module "html-react-parser" {
+  import { ReactNode } from "react";
+
+  export interface HTMLReactParserOptions {
+    replace?: (domNode: any) => ReactNode;
+    trim?: boolean;
+  }
+
+  export default function parse(html: string, options?: HTMLReactParserOptions): ReactNode;
+}

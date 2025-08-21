af6702d9511af7c74847b403461c9a445f77b92d
Daniel Anca <daniel.anca@eggs.ro>
2025-08-21 21:55:40 +0300

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,6 @@ const test = {
 } as UserConfig["test"];
 
 const isProd = process.env.NODE_ENV === "production";
-// Determine if HTTPS is being used
 const isHttps = process.env.USE_HTTPS === "true";
 const hmrProtocol = isHttps ? "wss" : "ws";
 
@@ -19,23 +18,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: parseInt("8000", 10),
-    https: isHttps, // Enable HTTPS if needed
-    hmr: {
-      protocol: hmrProtocol,
-      host: "diniubire.ro",
-      port: 24678,
-    },
+    https: isHttps,
+    hmr: { protocol: hmrProtocol, host: "diniubire.ro", port: 24678 },
   },
-  build: {
-    minify: isProd,
-    sourcemap: !isProd, // Enable source maps in development
-  },
+  build: { minify: isProd, sourcemap: !isProd },
   test,
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/client/styles/mixins.scss";\n`,
-      },
-    },
-  },
+  css: { preprocessorOptions: { scss: { additionalData: `@import "./src/client/styles/mixins.scss";\n` } } },
+
+  // 👇 aici vine fixul
+  optimizeDeps: { include: ["html-react-parser"] },
+  ssr: { noExternal: ["html-react-parser"] },
 });
